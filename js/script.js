6bce5b9734d37a48f085d6bcd6a18c0723b3e924
CJ Buys <cjstrattonbuys@gmail.com>
2022-02-25 11:23:47 -0500

    Fetched repo data

--- js/script.js
+++ js/script.js
@@ -2,11 +2,14 @@
 const overview = document.querySelector(".overview");
 // username
 const username = "cjstrattonbuys";
+// unordered list of repos
+const repoList = document.querySelector(".repo-list");
+
 
 const getGitHub = async function () {
     const request = await fetch (`https://api.github.com/users/${username}`);
     const gitHub = await request.json();
-    console.log(gitHub);
+    //console.log(gitHub);
     displayUserData(gitHub);
 }
 getGitHub();
@@ -25,4 +28,22 @@ const displayUserData = function (gitHub) {
             <p><strong>Number of public repos:</strong> ${gitHub.public_repos}</p>
         </div> `;
     overview.append(div);
+    fetchRepos();
 }
+
+const fetchRepos = async function () {
+    const request = await fetch (`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+    const repos = await request.json();
+    //console.log(repos);
+    displayRepos(repos);
+};
+fetchRepos();
+
+const displayRepos = function (repos) {
+    for (const repo of repos) {
+        const repoItem = document.createElement("li");
+        repoItem.classList.add("repo");
+        repoItem.innerHTML = `<h3>${repo.name}</h3>`;
+        repoList.append(repoItem);
+    }
+};
